808d11fa719b5ad09e579e55fed481b6d7239ff3
Peter Jang <peter.x.jang@gmail.com>
2019-10-28 21:19:01 -0500

    Use options hash instead of multiple initialize arguments for readability

--- employee.rb
+++ employee.rb
@@ -21,11 +21,11 @@ class Employee
   attr_reader :first_name, :last_name, :active
   attr_writer :active
 
-  def initialize(input_first_name, input_last_name, input_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
   end
 
   def print_info
@@ -35,23 +35,13 @@ class Employee
   def give_annual_raise
     @salary = @salary * 1.05
   end
-
-  # def first_name
-  #   @first_name
-  # end
-
-  # def first_name=(input_first_name)
-  #   @first_name = input_first_name
-  # end
 end
 
-employee1 = Employee.new("Majora", "Carter", 80000, true)
-employee2 = Employee.new("Danilo", "Campos", 70000, true)
+employee1 = Employee.new({ first_name: "Majora", last_name: "Carter", salary: 80000, active: true })
+employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
 employee1.print_info
 employee2.print_info
 employee2.give_annual_raise
 employee2.print_info
 
-p employee1.first_name
-employee1.first_name = "Peter"
 p employee1.first_name
