c9b23a95cdc4949502c8b1e8368e7af49632235d
Peter Jang <peter.x.jang@gmail.com>
2019-10-29 21:08:31 -0500

    Add custom attributes to Car and Bike classes using super to keep code DRY

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,5 +1,5 @@
 class Vehicle
-  def initialize
+  def initialize(input_options)
     @speed = 0
     @direction = "north"
   end
@@ -18,22 +18,38 @@ class Vehicle
 end
 
 class Car < Vehicle
+  def initialize(input_options)
+    super(input_options)
+    @make = input_options[:make]
+    @model = input_options[:model]
+    @fuel = 20
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  def initialize(input_options)
+    super(input_options)
+    @type = "Mountain"
+    @weight = 25
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-my_car = Car.new
+my_car = Car.new({ make: "Ford", model: "Explorer" })
 my_car.brake
 my_car.accelerate
 my_car.honk_horn
 p my_car
 
-my_bike = Bike.new
+my_car2 = Car.new({ make: "Honda", model: "Civic" })
+p my_car2
+
+my_bike = Bike.new({})
 my_bike.ring_bell
